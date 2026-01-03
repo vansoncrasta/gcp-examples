617a990467b80668a16e42ed86d1a812e4135880
Vanson Crasta <vansoncrasta@gmail.com>
2026-01-03 09:19:37 +0530

    Updates

--- gke-public-cluster-pub-sub/terraform/main.tf
+++ gke-public-cluster-pub-sub/terraform/main.tf
@@ -41,7 +41,6 @@ resource "google_container_cluster" "primary" {
   name               = "gke-public-cluster-example"
   location           = var.zone
   initial_node_count = 3
-  min_master_version = data.google_container_engine_versions.on-prem.latest_master_version
 
   // Enable Workload Identity
   workload_identity_config {
